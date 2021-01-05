88f3aa0e5c3fd8d4e5cdf8734855988539dcda99
Vlada-K <62649143+Vlada-K@users.noreply.github.com>
2021-01-05 09:44:38 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,37 @@
 // реализация пользовательского приложения
 
+#include<iostream>
+using namespace std;
+#include <string> 
+#include"arithmetic.h"
+
+
+
+
+
+
 int main()
 {
-  return 0;
+	string input;
+	while (true)
+	{
+		cout << " enter expression (or exit): ";
+		cin >> input;
+		
+		if (input == "exit")
+			break;
+		arithmetic W(input);
+		string output;
+		output =W.Process();
+		if (output == "")
+			cout<< " incorrect expression"<< endl;
+		else {
+			cout << output << endl;
+			cout << W.calculat() << endl;
+		}
+		
+	}
+	
+	return 0;
 }
+
